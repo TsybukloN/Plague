dfe1460b4aeea084d749d22ae359fa5123bb2027
Tsybulka Mikita <tsybulka.mikita.pl@gmail.com>
2024-03-20 09:44:12 +0100

    Add annimation for main charachter
    - Cerrectly horizontal orientation
    - Run(Bug fixing)
    - Shuffle(Sneaking)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,63 +1,84 @@
+using Cinemachine;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     private Animator animator;
     public CharacterController controller;
+    public CinemachineFreeLook freeLookCamera;
     public float walkSpeed;
     public float runSpeed;
+    public float sneakSpeed;
     public float smoothTime;
-    public float gravity = 9.81f;
-    float smoothVelocity;
-    public Transform firstCamera;
+
+    private float smoothVelocity;
+    private float horizontal;
+    private float vertical;
+    private float sneakingKey;
+    private float runningKey;
+
+    private Vector3 direction;
+
+    private const float gravity = 1000f;
 
     private void Start()
     {
+        Cursor.visible = false;
         animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        float running = Input.GetAxisRaw("Fire3"); // Fire3 представляет собой кнопку для бега (обычно левый шифт)
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        sneakingKey = Input.GetAxisRaw("Fire1"); // left ctrl for sneaking
+        runningKey = Input.GetAxisRaw("Fire3"); // left shift for running
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        float cameraAngle = freeLookCamera.State.CorrectedOrientation.eulerAngles.y;
+
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
             float currentSpeed;
-            if (running != 0f)
+
+            if (runningKey != 0f)
             {
                 currentSpeed = runSpeed;
-                animator.SetInteger("WalkIndecator", 2);
+                animator.SetInteger("WalkIndecator", 3);
+            }
+            else if (sneakingKey != 0f)
+            {
+                currentSpeed = sneakSpeed;
+                animator.SetInteger("WalkIndecator", 1);
             }
             else
             {
                 currentSpeed = walkSpeed;
-                animator.SetInteger("WalkIndecator", 1);
+                animator.SetInteger("WalkIndecator", 2);
             }
 
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + firstCamera.eulerAngles.y;
+            Quaternion lookRotation = Quaternion.Euler(0f, cameraAngle, 0f);
+            direction = lookRotation * direction;
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothVelocity, smoothTime);
+
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
-            if (controller.isGrounded)
-            {
-                moveDirection.y = 0f;
-            }
-            else
-            {
-                moveDirection.y -= gravity * Time.deltaTime;
-            }
-
-            controller.Move(moveDirection.normalized * currentSpeed * Time.deltaTime);
+            controller.Move(currentSpeed * Time.deltaTime * moveDirection.normalized);
         }
         else
         {
             animator.SetInteger("WalkIndecator", 0);
         }
+
+        if (!controller.isGrounded)
+        {
+            direction.y -= gravity * Time.fixedDeltaTime;
+            controller.Move(direction * Time.fixedDeltaTime);
+        }
     }
 }
-
