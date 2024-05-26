220624614ceb00dc2dc9cbf73a3b9f317d69377d
Tsybulka Mikita <nikita.tsybulka@gmail.com>
2024-05-26 16:59:39 +0200

    Quest 3 and UI Inventory

---
+++ Assets/Scripts/CollectItem.cs
@@ -0,0 +1,26 @@
+﻿using PixelCrushers.QuestMachine.Demo;
+using UnityEngine;
+
+public class HerbCollector : MonoBehaviour
+{
+    public Inventory inventory; // Ссылка на инвентарь
+
+    public void CollectNearbyItems(Collider hitCollider)
+    { 
+        if (hitCollider.CompareTag("Herb1"))
+        {
+            inventory.AddItem(Inventory.Herb1Slot);
+            Destroy(hitCollider.gameObject);
+        }
+        else if (hitCollider.CompareTag("Herb2"))
+        {
+            inventory.AddItem(Inventory.Herb2Slot);
+            Destroy(hitCollider.gameObject);
+        }
+        else if (hitCollider.CompareTag("Herb3"))
+        {
+            inventory.AddItem(Inventory.Herb3Slot);
+            Destroy(hitCollider.gameObject);
+        }
+    }
+}
