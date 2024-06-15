237c0eb01ee108f93259ed7633dbcfe52a634d95
Tsybulka Mikita <nikita.tsybulka@gmail.com>
2024-06-15 13:49:15 +0200

    Quest 4 starting

---
+++ Assets/Scripts/ItemCountQuestCondition.cs
@@ -0,0 +1,46 @@
+﻿namespace PixelCrushers.QuestMachine.Demo
+{
+    /// <summary>
+    /// This example condition for the Demo scene checks the amount of an item in the DemoInventory.
+    /// The Pirate's Coin Race quest will become unofferable if the player has already looted all
+    /// of the coins from the breakable crates and barrels.
+    /// </summary>
+    public class ItemCountQuestCondition : QuestCondition
+    {
+        public Inventory.ItemType itemType;
+
+        public CounterValueConditionMode comparison = CounterValueConditionMode.AtLeast;
+
+        public QuestNumber requiredValue = new QuestNumber();
+
+        public override string GetEditorName()
+        {
+            return "Inventory has " + comparison + " " + requiredValue.GetValue(quest) + " " + itemType;
+        }
+
+        public override void StartChecking(System.Action trueAction)
+        {
+            base.StartChecking(trueAction);
+            if (IsTrue()) SetTrue();
+        }
+
+        private bool IsTrue()
+        {
+            var demoInventory = FindObjectOfType<DemoInventory>();
+            if (demoInventory != null)
+            {
+                var itemCount = demoInventory.GetItemCount((int)itemType);
+                switch (comparison)
+                {
+                    case CounterValueConditionMode.AtLeast:
+                        if (itemCount >= requiredValue.GetValue(quest)) return true;
+                        break;
+                    case CounterValueConditionMode.AtMost:
+                        if (itemCount <= requiredValue.GetValue(quest)) return true;
+                        break;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/UpdateItemCounterQuestAction.cs
@@ -0,0 +1,38 @@
+﻿namespace PixelCrushers.QuestMachine.Demo
+{
+    /// <summary>
+    /// This example action for the Demo scene updates a quest counter with the 
+    /// current amount of items in the player's inventory. When the Harvest Carrots
+    /// quest starts, it uses this action to update the counter.
+    /// </summary>
+    public class UpdateItemCounterQuestAction : QuestAction
+    {
+        public Inventory.ItemType itemType;
+
+        public override string GetEditorName()
+        {
+            return "Update " + itemType.ToString().ToLower() + "s counter with current value in Inventory";
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            QuestCounter counter = null;
+            switch (itemType)
+            {
+                case Inventory.ItemType.Herb1:
+                    counter = quest.GetCounter("Herb1");
+                    if (counter != null) counter.currentValue = FindObjectOfType<DemoInventory>().GetItemCount((int)itemType);
+                    break;
+                case Inventory.ItemType.Herb2:
+                    counter = quest.GetCounter("Herb2");
+                    if (counter != null) counter.currentValue = FindObjectOfType<DemoInventory>().GetItemCount((int)itemType);
+                    break;
+                case Inventory.ItemType.Herb3:
+                    counter = quest.GetCounter("Herb3");
+                    if (counter != null) counter.currentValue = FindObjectOfType<DemoInventory>().GetItemCount((int)itemType);
+                    break;
+            }
+        }
+    }
+}
